ad088b39e529a9ab8d6bc944efecb588aaf4fb3b
Jiri Podivin <jpodivin@gmail.com>
2023-05-08 13:20:16 +0200

    Implementing prompt patterns
    Prompts can now be defined as yaml files in the 'prompt_patterns' dir.
    
    Basic QnA prompt was implemented as a sample.
    PyYaml was added among dependencies.
    Service now mounts 'prompt_patterns' dir as RO volume.
    Selected prompt pattern is maintained between model calls.
    
    Signed-off-by: Jiri Podivin <jpodivin@gmail.com>

--- pyama/prompt.py
+++ pyama/prompt.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, current_app, render_template, request, session
 from llama_cpp import Llama
 
-from pyama import download
+from pyama import download, constants
+
+from yaml import safe_load
+
+import glob
+import re
 
 bp = Blueprint('prompt', __name__)
 
@@ -34,10 +39,28 @@ def initialize_model(model_path, **kwargs):
         MODEL = dummy_model
 
 
-def get_settings():
+def get_prompt_patterns(prompt_path=constants.PROMPT_PATH):
+    patterns = {}
+    pattern_files = glob.glob(prompt_path+"/*.yaml")
+    current_app.logger.info(f"Discovered prompt patterns: {pattern_files}")
+    for pattern_file in pattern_files:
+        with open(pattern_file, 'r') as pattern_file:
+            pattern = safe_load(pattern_file.read())
+            patterns[pattern['name']] = pattern.copy()
+    return patterns
+
+
+def render_prompt(prompt_pattern, user_input):
+    prompt_pattern = re.sub('<PROMPT>', user_input, prompt_pattern)
+    prompt_pattern = re.sub('<RESPONSE>', '', prompt_pattern)
+    return prompt_pattern
+
+
+def get_settings(prompt_patterns):
 
     model_settings = request.form.copy()
     model_settings.pop('prompt')
+    selected_prompt = model_settings.pop('selected_prompt')
     model_settings.pop('selected_model')
     for key, value in request.form.items():
         if key in model_settings:
@@ -47,12 +70,19 @@ def get_settings():
                 model_settings.pop(key)
             else:
                 model_settings[key] = CONFIG_TYPE_CASTS[key](value)
+    model_settings['stop_strings'] = ','.join(prompt_patterns[selected_prompt]['stop_strings'])
 
     return model_settings
 
 
-def get_response(prompt='', debug=False, max_tokens=256, stop_strings=None, **kwargs):
+def get_response(prompt='', debug=False, max_tokens=256, stop_strings=None,
+                 prompt_pattern='', **kwargs):
     global MODEL
+    prompt = render_prompt(prompt_pattern, prompt)
+
+    current_app.logger.info(
+        f"Prompt rendered as: {prompt}")
+
     model_out = MODEL(prompt, max_tokens=max_tokens, stop=stop_strings, echo=True, **kwargs)
     if debug:
         return model_out
@@ -63,24 +93,37 @@ def get_response(prompt='', debug=False, max_tokens=256, stop_strings=None, **kw
 @bp.route('/prompts', methods=['GET', 'POST'])
 def prompts():
     available_models = download.get_models_list()
+    available_prompts = get_prompt_patterns()
     model_path = request.form.get('selected_model', "no_model")
+    selected_prompt = request.form.get('selected_prompt', "")
+
+    current_app.logger.info(f"Prompts {available_prompts}")
     current_app.logger.info(f"Selected model path: {model_path}")
+
     if 'model_settings' in session.keys():
         current_app.logger.info(f"Session settings {session['model_settings']}")
+    if 'selected_prompt' in session.keys():
+        current_app.logger.info(f"Selected prompt: {session['selected_prompt']}")
+
     if request.method == 'POST':
         if model_path == "no_model":
             current_app.logger.warning("No model selected")
             responses.append("No model selected!")
         else:
             initialize_model(model_path)
-            model_settings = get_settings()
+            model_settings = get_settings(available_prompts)
+
             session['model_settings'] = model_settings.copy()
+            session['selected_prompt'] = selected_prompt
             session.modified = True
+
+            selected_prompt = available_prompts[selected_prompt]
+
             current_app.logger.info(f"Current model settings: {model_settings}")
             responses.append(
                 get_response(prompt=request.form['prompt'],
-                             **model_settings))
+                             prompt_pattern=selected_prompt['pattern'], **model_settings))
 
     return render_template(
         'prompts.html', responses=reversed(responses), available_models=available_models,
-        model_settings=session.get('model_settings', {}))
+        model_settings=session.get('model_settings', {}), available_prompts=available_prompts)
